fee8a8946b8f10ea2197e258d1c803a4ff03e105
Dikesh3637 <dikeshreddy.dev@gmail.com>
2025-04-07 11:57:02 +0530

    initial status

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+	return (
+		<div className="border-red-200">
+			<Button variant="outline">Click</Button>
+		</div>
+	);
+}
+
+export default App;
