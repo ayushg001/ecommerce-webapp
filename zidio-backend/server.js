0dd63935f4dde7ea9a618cf9106f11d676c4bb6b
Ayush Gupta <ayushguptanonmedical@gmail.com>
2025-04-07 19:43:02 +0530

    initial backend setup

---
+++ zidio-backend/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();  
+const express = require('express');  
+const cors = require('cors');  
+const mongoose = require('mongoose');  
+const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
+
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+
+
+const app = express();  
+app.use(cors());  
+app.use(express.json());  
+
+mongoose.connect(process.env.MONGO_URI, {  
+  useNewUrlParser: true,  
+  useUnifiedTopology: true,  
+}).then(() => console.log("MongoDB Connected")).catch(err => console.log(err));  
+
+app.get("/", (req, res) => {  
+  res.send("Zidio Backend Running");  
+});  
+
+const PORT = process.env.PORT || 5000;  
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));  
